76b96c897c40c70abdf030a6903eaf386ff44be2
USER <rajputshubham615@gmail.com>
2023-01-24 11:18:09 +0530

    removed errors

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -83,41 +83,10 @@ export class RegistrationComponent {
     // this.profileForm.reset();
   }
 
-  uploadedImage: any;  
-  dbImage: any; 
-  postResponse: any;
-  successResponse: any;
-  image: any;
 
-  public onImageUpload(event:any) {    
-    this.uploadedImage = event.target.files[0];
-  }
-
-  imageUploadAction() {    
-    const imageFormData = new FormData();
-    imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
   
 
-    this.registrationService.saveImage(imageFormData).subscribe((response) => {
-        if (response === 200) { 
-          this.postResponse = response;                
-          this.successResponse = this.postResponse.body.message;
-        } else {
-          this.successResponse = 'Image not uploaded due to some error!';
-        }
-      }
-      );
-    }
-
-    viewImage() {
-      this.httpClient.get('http://localhost:9000/get/image/info/' + this.image)
-        .subscribe(
-          res => {
-            this.postResponse = res;          
-            this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
-          }
-        );
-    }
+   
 
   mustMatchValidator(fg: AbstractControl) {
     const passwordValue = fg.get("password")?.value;
